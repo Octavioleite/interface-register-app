e27453bb0697e0368339d7a1cb43187983b0e531
Octávio Leite Neto <127608953+Octavioleite@users.noreply.github.com>
2024-10-31 19:37:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [loading, setLoading] = useState(false); // Estado de carregamento
   const [error, setError] = useState(""); // Estado para mensagens de erro
 
-  const baseUrl = "https://api-register-users-cofr7psad-octavioleites-projects.vercel.app/?vercelToolbarCode=H9Upg_oM6vxH2f7";
+  const baseUrl = "https://api-register-users-gold.vercel.app/";
 
   const getUsers = async () => {
     setLoading(true);
