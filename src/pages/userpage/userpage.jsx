29149a881a6fd4582c51688c887b538f1fc6db3f
Octavio Leite Neto <otavio.gta@gmail.com>
2024-11-13 21:28:16 -0300

    update users page

--- src/pages/userpage/userpage.jsx
+++ src/pages/userpage/userpage.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navbar/navbar";
 import styles from "./userpage.module.css";
 
 const UserPage = ({ user }) => {
+  const [isModalOpen, setIsModalOpen] = useState(true); 
+
   if (!user) {
     return <p>Carregando...</p>;
   }
-  //className={styles.h1}
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <Navbar />
-      
       <h1>Bem-vindo, {user.name}</h1>
-      <p>teste</p> 
+      <center><p><strong>PAUTAS</strong></p></center>
+
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <span className={styles.closeButton} onClick={closeModal}>
+              &times;
+            </span>
+            <h1>Aviso Importante</h1>
+            <p className={styles.p}>
+              Os dados fornecidos serão utilizados exclusivamente para verificar a autenticidade do cadastro, garantindo total confidencialidade e segurança das informações.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
