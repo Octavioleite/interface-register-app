72200f3c52047d4c9aa6b58d7adbc6ac1fbe54c3
Octavio Leite Neto <otavio.gta@gmail.com>
2024-11-19 14:23:45 -0300

    update url

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     navigate('/users');
   };
 
-  const baseUrl = "http://localhost:3001";
+  const baseUrl = "https://api-register-users-gold.vercel.app";
   //const baseUrl = "https://api-register-users-gold.vercel.app";
   const getUsers = async () => {
     setLoading(true);
